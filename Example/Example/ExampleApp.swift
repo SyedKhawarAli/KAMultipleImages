f4acd7f2098d4bb43f900d8adaf0d235ae5e5681
Khawar Ali <khawarali5@gmail.com>
2024-03-13 04:31:51 +0200

    Example added

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by shah on 13.3.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
