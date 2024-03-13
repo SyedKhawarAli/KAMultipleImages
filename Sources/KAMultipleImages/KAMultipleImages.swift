cddc4f779acdda9752d9ac4ac74b011dd564bd72
Khawar Ali <khawarali5@gmail.com>
2024-03-13 04:30:04 +0200

    minimum version

--- Sources/KAMultipleImages/KAMultipleImages.swift
+++ Sources/KAMultipleImages/KAMultipleImages.swift
@@ -1,2 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct KAMultipleImages: View {
+    public init() {}
+    public var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
